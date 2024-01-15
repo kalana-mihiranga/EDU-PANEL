18c3ddccd71ba6a511dd304cf0511ee9c3f25989
Kalana Mihiranga <kalanamihiranga97@gmail.com>
2024-01-15 11:29:56 +0530

    firebase for save images

--- edupanel-backend/src/main/java/lk/ijse/dep11/edupanel/WebAppConfig.java
+++ edupanel-backend/src/main/java/lk/ijse/dep11/edupanel/WebAppConfig.java
@@ -1,13 +1,22 @@
 package lk.ijse.dep11.edupanel;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Bucket;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.StorageClient;
 import lk.ijse.dep11.edupanel.converter.LecturerTypeConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 @EnableWebMvc
@@ -21,6 +30,23 @@ public class WebAppConfig implements WebMvcConfigurer {
     @Bean
     public StandardServletMultipartResolver multipartResolver(){
         return new StandardServletMultipartResolver();
+
+    }
+
+    @Bean
+    public Bucket defaultBucket() throws IOException {
+
+        InputStream serviceAccount =
+                new ClassPathResource("/edupanel-60033-firebase-adminsdk-po2ms-74b355b4e8.json").getInputStream();
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setStorageBucket("edupanel-60033.appspot.com")
+                .build();
+
+        FirebaseApp.initializeApp(options);
+        return StorageClient.getInstance().bucket();
+
 
     }
 }
